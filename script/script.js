2f64ec475cdbab7de725ee5913cd50cee801449f
loveeyk <karir.loveleen@gmail.com>
2021-03-19 13:38:20 +0100

    implemented child classes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,53 @@
+//parent class Location
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 var Locations = /** @class */ (function () {
-    function Locations(lCity, lZipCode, lAdress, lImg, lRestName) {
+    function Locations(lCity, lZipCode, lAdress, lImg, lRestName, lDescription) {
         this.city = lCity;
         this.zipCode = lZipCode;
         this.adress = lAdress;
         this.img = lImg;
         this.restaurantName = lRestName;
+        this.description = lDescription;
     }
     Locations.prototype.display = function () {
-        return " <div class=\"card w-50 m-2\"><img class=\"card-img-top \" src=\"" + this.img + " \"></img> \n        <h2 class= \"card-title\">" + this.restaurantName + "</h2>\n        <p class= \"card-text\">Location: " + this.city + " : \" \"\n        <p class= \"card-text\">Color: " + this.adress + ", \" \" " + this.zipCode + "</p>\n        </div>";
+        return " <div class=\"\"><img class=\"\" src=\"" + this.img + " \"></img> \n        <h2 class= \"\">" + this.restaurantName + "</h2>\n        <p class= \"\">Speciality about us: " + this.description + "</p>\n        <p class= \"\">Location: " + this.city + " : \" \"\n       " + this.adress + ", \" \" " + this.zipCode + "</p>\n        </div>";
     };
     return Locations;
 }());
+// child class Restaurant 
+var restaurant = /** @class */ (function (_super) {
+    __extends(restaurant, _super);
+    function restaurant(lCity, lZipCode, lAdress, lImg, lRestName, lDescription, rCuisType, rNumber, rWeb) {
+        var _this = _super.call(this, lCity, lZipCode, lAdress, lImg, lRestName, lDescription) || this;
+        _this.cuisineType = rCuisType;
+        _this.telNumber = rNumber;
+        _this.webAdress = rWeb;
+        return _this;
+    }
+    restaurant.prototype.displayRestaurantInfos = function () {
+        return _super.prototype.display + ("<p class=\"\">Best greetings for " + this.cuisineType + " kitchen.</p>\n        <p class=\"\">For bookings and further information, do not hesitate to call on following number " + this.telNumber + "or leave us an e-mail " + this.webAdress + " </p>\n        ");
+    };
+    return restaurant;
+}(Locations));
+//child class Events
+var events = /** @class */ (function (_super) {
+    __extends(events, _super);
+    function events() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return events;
+}(Locations));
--- script/script.ts
+++ script/script.ts
@@ -1,27 +1,68 @@
+//parent class Location
+
 class Locations {
     restaurantName:string;
+    description:string;
     city:string;
     zipCode:number;
     adress:any;
     img:string;
 
-    constructor(lCity, lZipCode, lAdress, lImg, lRestName){
+    constructor(lCity, lZipCode, lAdress, lImg, lRestName, lDescription){
         this.city = lCity;
         this.zipCode = lZipCode;
         this.adress = lAdress;
         this.img = lImg;
         this.restaurantName = lRestName;
+        this.description = lDescription;
 
     }
 
     display(){
 
-        return ` <div class="card w-50 m-2"><img class="card-img-top " src="${this.img} "></img> 
-        <h2 class= "card-title">${this.restaurantName}</h2>
-        <p class= "card-text">Location: ${this.city} : " "
-        <p class= "card-text">Color: ${this.adress}, " " ${this.zipCode}</p>
+        return ` <div class=""><img class="" src="${this.img} "></img> 
+        <h2 class= "">${this.restaurantName}</h2>
+        <p class= "">Speciality about us: ${this.description}</p>
+        <p class= "">Location: ${this.city} : " "
+       ${this.adress}, " " ${this.zipCode}</p>
         </div>`
         
     }
+
+}
+
+// child class Restaurant 
+
+class restaurant extends Locations{
+    cuisineType:string;
+    telNumber:number;
+    webAdress:string;
+
+    constructor(lCity, lZipCode, lAdress, lImg, lRestName, lDescription, rCuisType, rNumber, rWeb){
+        super(lCity, lZipCode, lAdress, lImg, lRestName, lDescription)
+        this.cuisineType = rCuisType;
+        this.telNumber = rNumber;
+        this.webAdress = rWeb;
+
+
+    }
+
+    displayRestaurantInfos(){
+        return super.display + `<p class="">Best greetings for ${this.cuisineType} kitchen.</p>
+        <p class="">For bookings and further information, do not hesitate to call on following number ${this.telNumber}or leave us an e-mail ${this.webAdress} </p>
+        `
+    }
+
+    c
+
+    
+
+
+}
+
+//child class Events
+
+class events extends Locations{
+
 
 }
